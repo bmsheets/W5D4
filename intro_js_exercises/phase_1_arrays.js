f0e631a3a03516af32772dbc8a774e3bcfa6d8f4
christinesong <christinedisong@gmail.com>
2017-11-02 14:15:27 -0700

    Complete phase 1 arrays

---
+++ intro_js_exercises/phase_1_arrays.js
@@ -0,0 +1,52 @@
+Array.prototype.uniq = function() {
+  const result = [];
+  this.forEach(function(el) {
+    if (!result.includes(el)) {
+      result.push(el);
+    }
+  });
+  return result;
+};
+
+// var arr = [];
+// arr.push(1);
+// arr.push(2);
+// arr.push(2);
+// arr.push(2);
+// arr.push(3);
+//
+// console.log(arr);
+// arr = arr.uniq(); //arr should be [1, 2, 3]
+// console.log(arr);
+Array.prototype.twoSum = function() {
+  const result = [];
+  for(let i = 0; i < this.length; i++) {
+    for(let j = i+1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
+        result.push([i,j]);
+      }
+    }
+  }
+  return result;
+};
+
+// var arr = [1,2,2,-1,0,3];
+// console.log(arr);
+// arr = arr.twoSum();
+// console.log(arr);
+
+Array.prototype.transpose = function() {
+  const result = [];
+  for(let row = 0; row < this.length; row++) {
+    result.push([]);
+    for(let col = 0; col < this[row].length; col++) {
+      result[row][col] = this[col][row];
+    }
+  }
+  return result;
+};
+
+var arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(arr);
+arr = arr.transpose();
+console.log(arr);
